b2171f32d66ac7f8053c9e08c7cc760ffc7377c9
alejo <alejo@splice.com>
2019-04-29 18:05:25 -0500

    dont pass env vars

--- pubsub/pubsub.service.ts
+++ pubsub/pubsub.service.ts
@@ -3,10 +3,6 @@ import { GCLOUD_PROJECT } from '../consts/consts';
 const Pubsub = require('@google-cloud/pubsub');
 const pubsub = Pubsub({
   projectId: GCLOUD_PROJECT,
-  credentials: {
-    private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-  },
 });
 import { PubsubMessage } from './pubsub.types';
 
